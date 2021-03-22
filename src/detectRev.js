d11e75fd9c58b175ff5fe51210b3b7db3ecb168a
erzqy <riezqy189@gmail.com>
2021-03-23 01:17:33 +0800

    Change inline comment to block

--- src/detectRev.js
+++ src/detectRev.js
@@ -1,7 +1,7 @@
 var os = require("os");
 var platform = os.platform();
 var chromePlatform;
-const fetch = require("node-fetch");
+/* const fetch = require("node-fetch"); */
 
 switch (platform) {
     case "win32":
@@ -18,13 +18,15 @@ switch (platform) {
 
 this.getRevNumber = function () {
     return new Promise((resolve) => {
-        // fetch('https://omahaproxy.appspot.com/all.json?os=' + chromePlatform, { method: "get" })
-        //     .then(res => res.json())
-        //     .then(json => {
-        //         //console.log(json);
-        //         var betaChannel = json[0].versions.find(x => x.channel == "canary").branch_base_position;
+        /* 
+        fetch('https://omahaproxy.appspot.com/all.json?os=' + chromePlatform, { method: "get" })
+            .then(res => res.json())
+            .then(json => {
+                //console.log(json);
+                var betaChannel = json[0].versions.find(x => x.channel == "canary").branch_base_position; 
+        */
         resolve("666595");
-        // });
+        /* }); */
     })
 
 }
